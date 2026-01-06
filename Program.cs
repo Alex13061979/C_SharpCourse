d61cf93561dd33b1c73d1e1b76dcf537630bb737
Oleksandr Kherovymchuk <alexandr.kherovimchuk@gmail.com>
2026-01-06 16:06:01 +0200

    HW 4.1

--- Program.cs
+++ Program.cs
@@ -1,82 +1,31 @@
-﻿
-//Order1
-
-Console.WriteLine("Enter the order's number: ");
-int order = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Enter the client's name: ");
-string client = Console.ReadLine();
-
-Console.WriteLine("Enter the product's name: ");
-string productName = Console.ReadLine();
-
-Console.WriteLine("Enter the product's price: ");
-decimal productPrice = Convert.ToDecimal(Console.ReadLine());
-
-Console.WriteLine("Enter the client's street: ");
-string street = Console.ReadLine();
-
-Console.WriteLine("and the nomber of a house: ");
-int houseNum = int.Parse(Console.ReadLine());
-Console.WriteLine();
-
-//Another order
-
-Console.WriteLine("Enter the order's number: ");
-int order2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Enter the client's name: ");
-string client2 = Console.ReadLine();
-
-Console.WriteLine("Enter the product's name: ");
-string productName2 = Console.ReadLine();
-
-Console.WriteLine("Enter the product's price: ");
-decimal productPrice2 = Convert.ToDecimal(Console.ReadLine());
-
-Console.WriteLine("Enter the client's street: ");
-string street2 = Console.ReadLine();
-
-Console.WriteLine("and the nomber of a house: ");
-int houseNum2 = int.Parse(Console.ReadLine());
-Console.WriteLine();
-
-//And one more order
-
-Console.WriteLine("Enter the order's number: ");
-int order3 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Enter the client's name: ");
-string client3 = Console.ReadLine();
-
-Console.WriteLine("Enter the product's name: ");
-string productName3 = Console.ReadLine();
-
-Console.WriteLine("Enter the product's price: ");
-decimal productPrice3 = Convert.ToDecimal(Console.ReadLine());
-
-Console.WriteLine("Enter the client's street: ");
-string street3 = Console.ReadLine();
-
-Console.WriteLine("and the nomber of a house: ");
-int houseNum3 = int.Parse(Console.ReadLine());
-
+﻿Console.WriteLine("Enter the day's number from 1 to 7");
+string dayNumb = Console.ReadLine();
 Console.Clear();
 
-Console.WriteLine($"Order No: {order}");
-Console.WriteLine($"Client: {client}.");
-Console.WriteLine($"Product: {productName}, price: {productPrice} EUR.");
-Console.WriteLine($"Address: {street}, {houseNum}.");
-Console.WriteLine();
-
-Console.WriteLine($"Order No: {order2}");
-Console.WriteLine($"Client: {client2}.");
-Console.WriteLine($"Product: {productName2}, price: {productPrice2} EUR.");
-Console.WriteLine($"Address: {street2}, {houseNum2}.");
-Console.WriteLine();
-
-Console.WriteLine($"Order No: {order3}");
-Console.WriteLine($"Client: {client3}.");
-Console.WriteLine($"Product: {productName3}, price: {productPrice3} EUR.");
-Console.WriteLine($"Address: {street3}, {houseNum3}.");
-Console.WriteLine();
+switch (dayNumb)
+{
+    case "1":
+        Console.WriteLine("It's Monday");
+        break;
+    case "2":
+        Console.WriteLine("It's Tuesday");
+        break;
+    case "3":
+        Console.WriteLine("It's Wednsday");
+        break;
+    case "4":
+        Console.WriteLine("It's Thursday");
+        break;
+    case "5":
+        Console.WriteLine("It's Friday");
+        break;
+    case "6":
+        Console.WriteLine("It's Saturday");
+        break;
+    case "7":
+        Console.WriteLine("It's Sunday");
+        break;
+    default:
+        Console.WriteLine("You've entered the wrong number!");
+        break;
+}
